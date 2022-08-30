b21a15a78a453a1df961e76adf69f922972aa04f
Tsumoridesu <tsumoridesu@gmail.com>
2022-08-30 15:24:55 +0900

    add pubilsh

--- scripts/test_for_alg.py
+++ scripts/test_for_alg.py
@@ -1,4 +1,4 @@
-# !/usr/bin/env python
+# !/usr/bin/env python3
 # coding = utf-8
 from tf import transformations
 import yaml
@@ -10,28 +10,32 @@ from yolov5_pytorch_ros.msg import BoundingBoxes
 import numpy as np
 import math
 from geometry_msgs.msg import PoseArray
-
+from std_msgs.msg import Float64MultiArray
 
 
 
 class landmark_location:
     def __init__(self):
+
         self.boundingboxes = []
         self.path = "/home/tsumori/catkin_ws/src/landmark_location/landmark_cfg/landmark_list.yaml"
         with open(self.path, "r", encoding="utf-8") as file:
             self.landmark_list = yaml.load(file, Loader=yaml.FullLoader)
         rospy.Subscriber("/detected_objects_in_image", BoundingBoxes, self.cb_image)
         rospy.Subscriber("/particlecloud", PoseArray, self.cb_particle)
+        self.pub = rospy.Publisher("/vision_weight", Float64MultiArray, queue_size=1)
+        self.vision_weight = []
+        self.vision_weight_pub = Float64MultiArray()
 
     def cb_particle(self, data):
         for i in self.boundingboxes:
             if i.Class == "Vending machine":
-                print(i.yaw)
                 for j in data.poses:
                     (r, p, y) = transformations.euler_from_quaternion(
                         [j.orientation.x, j.orientation.y, j.orientation.z, j.orientation.w])
-                    phi = math.atan2((self.landmark_list['landmark'][0]["Vending machine"][0]['pose'][1] - j.position.y),
-                                       self.landmark_list['landmark'][0]["Vending machine"][0]['pose'][0] - j.position.x) + y
+                    phi = math.atan2(
+                        (self.landmark_list['landmark'][0]["Vending machine"][0]['pose'][1] - j.position.y),
+                        self.landmark_list['landmark'][0]["Vending machine"][0]['pose'][0] - j.position.x) + y
                     if phi > math.pi:
                         phi -= 2 * math.pi
                     elif phi < -math.pi:
@@ -39,14 +43,17 @@ class landmark_location:
 
                     theta = phi - i.yaw
                     weight = (math.cos(theta) + 1) * i.probability
-                    print(theta, weight)
-    def cb_image(self, data):
+                    self.vision_weight.append(weight)
+            else:
+                self.vision_weight_pub = Float64MultiArray()
+        self.vision_weight_pub = Float64MultiArray(data=self.vision_weight)
+        self.pub.publish(self.vision_weight_pub)
 
+    def cb_image(self, data):
         self.boundingboxes = data.bounding_boxes
 
 
-
 if __name__ == '__main__':
-    rospy.init_node('test')
+    rospy.init_node('landmark_localization')
     landmark_location()
     rospy.spin()
